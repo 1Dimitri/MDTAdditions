e2b6112bd7f0bdac4d52a292a067deff6f419f3f
1Dimitri <git.dimitri@janczak.net>
2017-06-30 15:51:53 +0200

    IE 11 patches

--- Build-ISO.ps1
+++ Build-ISO.ps1
@@ -27,24 +27,25 @@ param(
   Set-ItemProperty -Path $ImagePath -name IsReadOnly -value $false
   $MountedPath = 'E:\MountDir'
   $LogDir ='Q:\Logs'
+  $TempDir = 'Q:\Temp'
  
-  $MountedPath,$LogDir | Remove-Item -Force -Recurse
-  $MountedPath,$LogDir | ForEach-Object { New-Item  -ItemType Directory -Path $_ }
+  $MountedPath,$LogDir,$TempDir | Remove-Item -Force -Recurse
+  $MountedPath,$LogDir,$TempDir | ForEach-Object { New-Item  -ItemType Directory -Path $_ }
 
   # Passing all packages in the folder at once doesn't work e.g. KB 3125574 after servicing stack
   # Add-WindowsPackage -PackagePath $PatchesDir -Path $MountedPath -LogPath $LogPath -NoRestart
   Get-ChildItem $PatchesDir -REcurse | Sort-Object Name | ForEach-Object {
    $LogPath = Join-Path $LogDir "$($_.Name).log"
-    Mount-WindowsImage -ImagePath $ImagePath -Index $ImageIndex -Path $MountedPath -LogPath $LogPath 
-    Add-WindowsPackage -PackagePath $_.FullName -Path $MountedPath -LogPath $LogPath -NoRestart
-    Dismount-WindowsImage -Path $MountedPath -Save -LogPath $LogPath
+    Mount-WindowsImage -ImagePath $ImagePath -Index $ImageIndex -Path $MountedPath -LogPath $LogPath -ScratchDirectory $TempDir
+    Add-WindowsPackage -PackagePath $_.FullName -Path $MountedPath -LogPath $LogPath -NoRestart -ScratchDirectory $TempDir
+    Dismount-WindowsImage -Path $MountedPath -Save -LogPath $LogPath -ScratchDirectory $TempDir
 
   }
   
 }
 
-#EXAMPLE:
-$urls = (Import-CSV 'E:\patcheslist\win2008r2-patches-201706.csv' -Header URL).URL
-Get-PatchesFromURL $urls -TargetPath 'E:\downloads'
-Apply-PatchesToIndexImage -PatchesDir 'E:\downloads' -ImagePath Q:\install.wim -ImageIndex 1
+$urls = (Import-CSV 'E:\patchesround\win2008r2-patches-201706.csv' -Header URL).URL
+Get-PatchesFromURL $urls -TargetPath 'E:\downloads2'
+Apply-PatchesToIndexImage -PatchesDir 'E:\downloads2' -ImagePath Q:\install.wim -ImageIndex 1
+
 
